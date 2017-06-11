e11a2b40a93c1964a108b11b51a2fbaba261b901
ankushjamdagani <anqush.jamdagani@gmail.com>
2017-06-12 01:42:50 +0530

    design changes

--- src/views/loading-page/loading-page.jsx
+++ src/views/loading-page/loading-page.jsx
@@ -10,9 +10,15 @@ class LoadingPage extends React.Component {
 		let launchTime = Date.parse('July 1, 2017');
 
 		return (
-			<div>
+			<div className="app">
 				<Loader />
+				<h1 className="heading">Comming Soon !!</h1>
 				<Timer to={launchTime} from={today} interval={1000}/>
+				<div className="footer">
+					<div></div>
+					<div></div>
+					<div></div>
+				</div>
 			</div>
 		)
 	}
