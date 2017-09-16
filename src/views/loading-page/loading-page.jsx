f27f8006ac2e25a5ffaaf3b82bae2bab54e2f08b
ankushjamdagani@gmail.com <anqush.jamdagani@gmail.com>
2017-09-17 02:00:47 +0530

    window content - with issue multiple defect in state transition

--- src/views/loading-page/loading-page.jsx
+++ src/views/loading-page/loading-page.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { Loader, Timer } from '../../components';
 
 class LoadingPage extends React.Component {
-	
+
 	render() {
 		let today = new Date();
 		let launchTime = Date.parse('July 1, 2017');
@@ -13,7 +13,7 @@ class LoadingPage extends React.Component {
 			<div className="app">
 				<Loader />
 				<h1 className="heading">Comming Soon !!</h1>
-				<Timer to={launchTime} from={today} interval={1000}/>
+				{/* <Timer to={launchTime} from={today} interval={1000}/> */}
 				<div className="footer">
 					<div></div>
 					<div></div>
