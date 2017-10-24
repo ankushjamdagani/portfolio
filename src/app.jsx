4fad9c2e474e99d116330da6b9e2875c24fe6d33
ankushjamdagani <anqush.jamdagani@gmail.com>
2017-10-25 01:36:09 +0530

    routes removed from this branch

--- src/app.jsx
+++ src/app.jsx
@@ -13,8 +13,12 @@ const App = ({store}) => (
 					<li><Link to="/portfolio/">Home</Link></li>
 					<li><Link to="/portfolio/loading">Loading</Link></li>
 				</ul>*/}
-				<Route exact path="/" component={CoverPage} />
-				<Route path="/portfolio/loading" component={LoadingPage} />
+
+				{/*
+					Yeah this is not exact match for the route. For all routes the CoverPage will be rendered
+				*/}
+				<Route path="/" component={CoverPage} />
+				{/*<Route path="/portfolio/loading" component={LoadingPage} />*/}
 			</div>
 	    </Router>
 	</Provider>
